c716a9951f4e72eddd8119775b65325714cb2951
Alejandro Cardenas <alejandro.cardenas_g@uao.edu.co>
2022-02-21 20:02:22 -0500

    1st Commit

---
+++ useCounter.js
@@ -0,0 +1,32 @@
+import { setSelectionRange } from '@testing-library/user-event/dist/utils';
+import React, { useState } from 'react';
+
+export const useCounter = (initialState = 10) => {
+
+    const [state, setState] = useState(initialState);
+
+    const increment = (factor = 1) => {
+        setState(state + factor);
+    }
+
+    const decrement = (factor = 1) => {
+        setState(state - factor);
+    }
+
+    const reset = () => {
+
+        setState(initialState);
+
+    }
+
+    return {
+
+        state,
+        increment,
+        decrement,
+        reset
+
+    };
+
+
+};
